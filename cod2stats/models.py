b8ce23f40f07beddb8fc481d07db2c07796a8746
Joakim Saario <s@xyr.se>
2014-07-10 23:18:16 +0200

    added some functions to models

--- cod2stats/models.py
+++ cod2stats/models.py
@@ -7,11 +7,17 @@ class Model:
     def __init__(self, db_settings=None):
         if not db_settings:
             db_settings = getattr(settings, 'DATABASE_SETTINGS',
-                                  {'db': 'default',
+                                  {'db': 'cod2stats',
                                    'user': 'postgres',
                                    'pw': ''})
 
         self.db = web.database(dbn='postgres', **db_settings)
 
+    def all(self, table):
+        return self.db.select(table)
 
-    # Future stuff
+    def by_id(table¸ id):
+        return self.db.where(table, id=id)
+
+    def filter(self, table, **kwargs):
+        return self.db.where(table, **kwargs)
