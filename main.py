dee6747b4e579629c37fbcf3d214c707d3caa203
Joakim Saario <s@xyr.se>
2014-07-15 01:33:08 +0200

    added wsgi identifier

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import web
 
 from cod2stats.urls import urls
 
+app = web.application(urls, globals())
+application = app.wsgifunc()
+
 if __name__ == "__main__":
-    app = web.application(urls, globals())
     app.run()
