c4a3b07eba4751c96432e06ca09cd5479343fffd
Joakim Saario <s@xyr.se>
2014-07-14 02:03:43 +0200

    synced database setup with https://github.com/kqr/cod2commitstats

--- contrib/postgres-tables.sql
+++ contrib/postgres-tables.sql
@@ -1,8 +1,13 @@
+-- To delete:
+-- DROP TABLE players CASCADE; DROP TABLE rounds CASCADE; DROP TABLE roundplayers CASCADE; DROP TABLE deaths CASCADE;
+
 CREATE TABLE players
 (
   id serial PRIMARY KEY,
   name text NOT NULL UNIQUE,
-  playtime interval DEFAULT '0 S'
+  playtime interval DEFAULT '0 S',
+  kills int NOT NULL DEFAULT 0,
+  deaths int NOT NULL DEFAULT 0
 );
 
 CREATE TABLE rounds
@@ -16,6 +21,8 @@ CREATE TABLE roundplayers
   id serial PRIMARY KEY,
   round_id int REFERENCES rounds(id),
   player_id int REFERENCES players(id),
+  kills int NOT NULL,
+  deaths int NOT NULL,
   playtime interval NOT NULL
 );
 
@@ -25,6 +32,8 @@ CREATE TABLE deaths
   round_id int REFERENCES rounds(id),
   dead_id int REFERENCES players(id),
   killer_id int REFERENCES players(id),
-  weapon text NOT NULL
+  weapon text NOT NULL,
+  killfeed text NOT NULL,
+  headshot boolean NOT NULL
 );
 
